f0ae3275ed8cbf137aa5465cf0d92a289ed825bb
chengchao <865488684@qq.com>
2024-01-21 18:38:24 +0800

    chart

--- Maxmoo/Project/Study/Controller/Chart/CRSChart/CRSTrackClimbChartView.swift
+++ Maxmoo/Project/Study/Controller/Chart/CRSChart/CRSTrackClimbChartView.swift
@@ -26,14 +26,25 @@ struct CRSTrackClimbInfo {
 
 class CRSTrackClimbChartView: UIView {
 
-    var trackInfos: [CRSTrackClimbInfo] = [] {
+    public var trackInfos: [CRSTrackClimbInfo] = [] {
         didSet {
             refreshChart()
         }
     }
     
     // 图表距离边界的距离
-    var chartInset: UIEdgeInsets = UIEdgeInsets(top: 10, left: 40, bottom: 30, right: 16)
+    public var chartInset: UIEdgeInsets = UIEdgeInsets(top: 10, left: 40, bottom: 30, right: 16) {
+        didSet {
+            refreshChart()
+        }
+    }
+    
+    // 高亮的index范围
+    public var highlightRange: Range = Int(Int.max - 1)..<Int(Int.max) {
+        didSet {
+            drawLineChart()
+        }
+    }
     
     // X轴最大值
     private var maxX: Double = -Double.greatestFiniteMagnitude
@@ -64,10 +75,10 @@ class CRSTrackClimbChartView: UIView {
     private var lineChartLayer: CAShapeLayer = {
         let chartLayer = CAShapeLayer()
         chartLayer.fillColor = UIColor.clear.cgColor
-        chartLayer.lineWidth = 3.0
+        chartLayer.lineWidth = 1.0
         chartLayer.lineCap = .round
         chartLayer.lineJoin = .round
-        chartLayer.strokeColor = UIColor.purple.cgColor
+        chartLayer.strokeColor = UIColor.clear.cgColor
         return chartLayer
     }()
     
@@ -104,15 +115,33 @@ class CRSTrackClimbChartView: UIView {
     
     // 画平滑曲线
     private func drawLineChart() {
-        if let colorSublasyers = colorRectLayer.sublayers {
-            for subLayer in colorSublasyers {
+        if let colorSublayers = colorRectLayer.sublayers {
+            for subLayer in colorSublayers {
                 subLayer.removeFromSuperlayer()
             }
         }
 
+        if let lineSublayers = lineChartLayer.sublayers {
+            for subLayer in lineSublayers {
+                subLayer.removeFromSuperlayer()
+            }
+        }
+        
         let drawWidth = lineChartLayer.width
         let drawHeigh = lineChartLayer.height
         var drawPoint = CGPoint.zero
+        
+        // highlight 边界竖线
+        var highStartIndex: Int = -2
+        var highEndIndex: Int = -1
+        if trackInfos.count > 0, highlightRange.endIndex > highlightRange.startIndex {
+            let countRange = 0..<trackInfos.count
+            if countRange.contains(highlightRange.startIndex) &&
+                countRange.contains(highlightRange.endIndex - 1) {
+                highStartIndex = highlightRange.startIndex
+                highEndIndex = highlightRange.endIndex - 1
+            }
+        }
         
         var lastPoint: CGPoint = .zero
         var lastRectHei: CGFloat = -CGFloat.greatestFiniteMagnitude
@@ -131,8 +160,30 @@ class CRSTrackClimbChartView: UIView {
             let rect = CGRect(x: lastPoint.x, y: colorRectLayer.height - finalRectH, width: x - lastPoint.x, height: finalRectH)
             let rectLayer = CAShapeLayer()
             rectLayer.frame = rect
-            rectLayer.backgroundColor = UIColor.randomColor.cgColor
+            rectLayer.backgroundColor = color(level: info.level).cgColor
             colorRectLayer.addSublayer(rectLayer)
+            
+            // not highlight
+            if !highlightRange.contains(index) {
+                let darkRectLayer = CAShapeLayer()
+                darkRectLayer.frame = rect
+                darkRectLayer.backgroundColor = UIColor(white: 0.3, alpha: 0.8).cgColor
+                colorRectLayer.addSublayer(darkRectLayer)
+            }
+            
+            // highlight
+            if index == highStartIndex {
+                let hx = rect.x - 0.25
+                let vLayer = vDottedLine(at: hx, color: .blue, height: colorRectLayer.height)
+                lineChartLayer.addSublayer(vLayer)
+            }
+            
+            if index == highEndIndex {
+                let hx = rect.x + rect.width - 0.25
+                let vLayer = vDottedLine(at: hx, color: .blue, height: colorRectLayer.height)
+                lineChartLayer.addSublayer(vLayer)
+            }
+            
             lastRectHei = finalRectH
             lastPoint = drawPoint
 
@@ -151,15 +202,6 @@ class CRSTrackClimbChartView: UIView {
         path.addLine(to: CGPoint(x: 0, y: lineChartLayer.height))
         maskLayer.path = path.cgPath
         colorRectLayer.mask = maskLayer
-        
-//        let animation = CABasicAnimation(keyPath: "strokeEnd")
-//        animation.fromValue = 0
-//        animation.toValue = 3
-//        animation.autoreverses = false
-//        animation.duration = 3
-//        
-//        lineChartLayer.add(animation, forKey: nil)
-//        lineChartLayer.strokeEnd = 1
     }
     
 }
@@ -180,4 +222,45 @@ extension CRSTrackClimbChartView {
         }
     }
     
+    private func color(level: Int?) -> UIColor {
+        switch level {
+        case 1:
+            return .red
+        case 2:
+            return .yellow
+        case 3:
+            return .purple
+        case 4:
+            return .green
+        case 5:
+            return .brown
+        default:
+            return .black
+        }
+    }
+}
+
+// MARK: - create ui
+extension CRSTrackClimbChartView {
+    // 竖直线
+    @discardableResult
+    func vDottedLine(at x: CGFloat,
+                        color: UIColor,
+                        height: CGFloat) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        layer.frame = CGRect(x: x - 0.5, y: 0, width: 1, height: height)
+        layer.lineWidth = 1.0
+        layer.lineDashPattern = [4,2]
+        layer.fillColor = UIColor.clear.cgColor
+        layer.strokeColor = color.cgColor
+        
+        let path = CGMutablePath()
+        path.move(to: CGPoint(x: 0.5, y: 0))
+        path.addLine(to: CGPoint(x: 0.5, y: layer.bounds.height))
+        
+        layer.path = path
+        self.layer.addSublayer(layer)
+        
+        return layer
+    }
 }
--- Maxmoo/Project/Study/Controller/Chart/ChartViewController.swift
+++ Maxmoo/Project/Study/Controller/Chart/ChartViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class ChartViewController: UIViewController {
 
+    lazy var button: UIButton = {
+        let bu = UIButton(frame: CGRect(x: 16, y: 100, width: 100, height: 40))
+        bu.backgroundColor = .randomColor
+        bu.addTarget(self, action: #selector(refreshChart), for: .touchUpInside)
+        return bu
+    }()
+    
     lazy var chart: CRSTrackClimbChartView = {
-        let chart = CRSTrackClimbChartView(frame: CGRect(x: 16, y: 100, width: kScreenWidth - 32, height: 180))
+        let chart = CRSTrackClimbChartView(frame: CGRect(x: 16, y: 200, width: kScreenWidth - 32, height: 180))
         chart.backgroundColor = .randomColor
         return chart
     }()
@@ -19,8 +26,17 @@ class ChartViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        view.addSubview(button)
         view.addSubview(chart)
         chart.trackInfos = CRSTrackClimbInfo.testInfos()
+        chart.highlightRange = 12..<17
     }
 
+    
+    @objc
+    func refreshChart() {
+        let start = Int.random(in: 0..<10)
+        let gap = Int.random(in: 1..<9)
+        chart.highlightRange = start..<(start+gap)
+    }
 }
